8f910a78b830aecf28c61b281637f4aca38c53da
Ldrsfurkan <furkangolemez@gmail.com>
2024-07-30 23:05:30 +0300

    unused scripts removed

---
+++ Assets/Scripts/PackageManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageManager : MonoBehaviour
+{
+    public GameObject package;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
