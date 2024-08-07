b1970eaf1efadf057a8c95eeb3ffc7713b0e043e
Ldrsfurkan <furkangolemez@gmail.com>
2024-08-07 20:05:07 +0300

    Path System and movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,15 @@ using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Transform[] pathParents;
     public GameObject parentObject;  
+    public int pathId = 0;
+     Vector3[][] paths;
+     int currentPointIndex = 0;
+     bool isMoving = false;
+
     public int collectedCount = 0;
-    public float speed = 20.0f;
+    public float speed = 5.0f;
     public bool isGameEnd = false;
     public bool isDead = false;
     public bool isDamaged = false;
@@ -38,23 +43,66 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
          parentObject.transform.localPosition = new Vector3 (0,0,-1);
+         PathCreate();
     }
     void Update()
     {
         Movement();     
     }
 
-    void Movement(){
+    void Movement()
+    {
         if(!isGameEnd && !isDead)
         { 
-            if(Input.GetKey(KeyCode.Space))
-            { 
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+             if (Input.GetKey(KeyCode.Space))
+        {
+            if (!isMoving)
+            {
+                isMoving = true;
+                MoveToNextPoint();
+            }
+        }
+        else
+        {
+            isMoving = false;
+            transform.DOKill(); // Hareketi durdur
+        }
+        }
+    }
+     private void MoveToNextPoint()
+    {
+        if (currentPointIndex < paths[pathId].Length)
+        {
+            Vector3 nextPoint = paths[pathId][currentPointIndex];
+            float distance = Vector3.Distance(transform.position, nextPoint);
+            float duration = distance / speed;
+
+            transform.DOMove(nextPoint, duration).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                if (isMoving)
+                {
+                    currentPointIndex++;
+                    MoveToNextPoint();
+                }
+            });
+        }
+    }
+void PathCreate()
+    {
+        // Path noktalarını pathParents'ın child objelerinden al
+        paths = new Vector3[pathParents.Length][];
+        for (int i = 0; i < pathParents.Length; i++)
+        {
+            Transform[] pathTransforms = pathParents[i].GetComponentsInChildren<Transform>();
+            paths[i] = new Vector3[pathTransforms.Length - 1];
+            for (int j = 1; j < pathTransforms.Length; j++)
+            {
+                paths[i][j - 1] = pathTransforms[j].position; // İlk eleman pathParent olduğundan atlanır
             }
         }
     }
-
-    public void GetDamaged()
+        
+    public void GetDamaged() 
     {
         if (!isDamaged)
         {
@@ -77,7 +125,6 @@ public class PlayerController : MonoBehaviour
             } 
         }
     }
-    
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Collectable"))
@@ -117,5 +164,5 @@ public class PlayerController : MonoBehaviour
         isDamaged = false;
         gameObject.transform.position = new Vector3(-1,0,0); 
     }
-        
+  
 }
